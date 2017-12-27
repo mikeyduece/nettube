b8bc005a73d892876142e5d38e06a39e2c2eda32
Mike Heft <mikeheft@gmail.com>
2017-12-27 15:15:15 -0700

    Side nav style

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -4,25 +4,29 @@ import './SideBar.css'
 export default class SideBar extends Component {
   render(){
     return(
-      <ul className='mainmenu'>
-        <li>
-          <a href="">home</a>
-        </li>
-        <li>
-          <a href="">favorites</a>
-        </li>
-        <li>
-          <a href="">playlists</a>
-            <ul className='submenu'>
-            <li>
-              <a href="">list of lists</a>
-            </li>
-            </ul>
-        </li>
-        <li>
-          <a href="">friends</a>
-        </li>
-      </ul>
+      <nav className='naviagtion'>
+        <ul className='mainmenu'>
+          <li>
+            <a href="">home</a>
+          </li>
+          <li>
+            <a href="">favorites</a>
+          </li>
+          <li>
+            <a href="">playlists</a>
+              <ul className='submenu'>
+              <li>
+                <a href="">
+                  <strong>list of lists</strong>
+                </a>
+              </li>
+              </ul>
+          </li>
+          <li>
+            <a href="">friends</a>
+          </li>
+        </ul>
+      </nav>
     )
   }
 }
