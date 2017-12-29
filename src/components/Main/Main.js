5cb2f29d7e6458fa3924c1efd17702c822a0db82
Mike Heft <mikeheft@gmail.com>
2017-12-29 13:05:18 -0700

    Work on displaying videos

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react'
+import Video from '../Video/Video'
 
 export default class Main extends Component {
-  constructor(props) {
-    super(props)
-  }
+
   render(){
     return(
-      <div>
-        <h1 className='user-home'> Main Page </h1>
-      </div>
+      <ul className='cards'>
+      {this.props.videos.map((video,index) => {
+              return <Video key={index} {...video} />
+           })}
+      </ul>
     )
   }
 }
--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react'
+import './Video.css'
 
 export default class Video extends Component {
   constructor(props) {
-    debugger
     super(props)
   }
   render() {
     return(
-      <div>
-        <h1> VIDEOS </h1>
-
-      </div>
+      <li className="cards__item">
+        <div className="card">
+          <div className="card__image card__image--fence"></div>
+          <div className="card__content">
+            <div className="card__title">Flex</div>
+            <p className="card__text"></p>
+            <button className="btn btn--block card__btn">Button</button>
+          </div>
+        </div>
+      </li>
     )
   }
 }
