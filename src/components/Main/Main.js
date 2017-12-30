e4f79e73ddd60df9669ecc2141292dcd0ea380d5
Mike Heft <mikeheft@gmail.com>
2017-12-30 09:23:20 -0700

    Style Modal

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,72 @@
 import React, {Component} from 'react'
 import Video from '../Video/Video'
+import Modal from 'react-modal'
 import './Main.css'
 
 export default class Main extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: false,
+    }
+
+    this.openModal = this.openModal.bind(this)
+    // this.afterOpenModal = this.afterOpenModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  componentWillMount() {
+    Modal.setAppElement('body')
+  }
+
+  // afterOpenModal() {
+  //   // references are now sync'd and can be accessed.
+  //   this.subtitle.style.color = '#f00';
+  // }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+
+  handleModalPlay(id){
+    this.props.handleModalPlay(id)
+  }
 
   render(){
     return(
-      <div className='cards'>
-        {this.props.videos.map((video,index) => {
-          return <Video key={index} {...video} />
-         })}
+      <div className='main-body'>
+        <div className='cards'>
+          {this.props.videos.map((video,index) => {
+            return <Video videoId={this.handleModalPlay.bind(this)}
+            key={index} {...video} />
+          })}
+        </div>
+        <div>
+          <button onClick={this.openModal}>Open Modal</button>
+            <Modal
+              className='modal'
+              isOpen={this.state.modalIsOpen}
+              // onAfterOpen={this.afterOpenModal}
+              onRequestClose={this.closeModal}
+              contentLabel='Example Modal'>
+                <h2>Hello</h2>
+                <button onClick={this.closeModal}>close</button>
+                <div>I am a modal</div>
+                <form>
+                  <input />
+                  <button>tab navigation</button>
+                  <button>stays</button>
+                  <button>inside</button>
+                  <button>the modal</button>
+                </form>
+            </Modal>
+        </div>
       </div>
     )
   }
--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -6,6 +6,11 @@ export default class Video extends Component {
     super(props)
   }
 
+  getVideoId(e){
+    e.preventDefault()
+    let vid = e.target.closest('div[class=card]').id
+  }
+
   image(){
     if(this.props.img_high !== undefined){
       return this.props.img_high
@@ -16,13 +21,14 @@ export default class Video extends Component {
 
   render() {
     return(
-        <div className="card">
+        <div id={this.props.vid_id} className="card">
           <img className='video-still' src={this.image()} alt="Video Screenshot" />
           <div className="card-content">
             <div className="card-title">{this.props.title}</div>
             <p className="card-text">{this.props.description}</p>
-            <i className="fa fa-play" aria-hidden="true"></i>
+            <i onClick={this.getVideoId.bind(this)} className="fa fa-play" aria-hidden="true"></i>
           </div>
+
         </div>
     )
   }
